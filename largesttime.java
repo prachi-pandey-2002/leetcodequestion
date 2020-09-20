c811877449d870ecadf643c334f4412c2f1ffa93
prachi <prachiipandey2002@gmail.com>
2020-09-20 21:41:05 +0530

    committed successsfully

---
+++ largesttime.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String largestTimeFromDigits(int[] A) {
+        String ans="";
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<A.length;j++){
+                for(int k=0;k<A.length;k++){
+                    if(i==j||i==k||j==k){
+                        continue;
+                    }
+                    int l =6-i-j-k;
+                    String h = ""+A[i]+A[j];
+                    String n = ""+A[k]+A[l];
+                    String t= h+":"+n;
+                    if(h.compareTo("24")<0 && n.compareTo("60")<0 && t.compareTo(ans)>0){
+                        ans = t;
+                    }
+                }
+            }
+        }
+     return ans;   
+    }
